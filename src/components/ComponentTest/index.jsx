b3ceba110289c5c222bdd034a883526c9afddc9e
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-26 22:34:18 -0300

    Teste de pages e components

---
+++ src/components/ComponentTest/index.jsx
@@ -0,0 +1,8 @@
+export default function ComponentTest() {
+  return(
+    <div>
+      <h2>Testando um componente</h2>
+      <p>Maciel mongol</p>
+    </div>
+  );
+}
---
+++ src/pages/PageTest/index.jsx
@@ -0,0 +1,10 @@
+import ComponentTest from "./components/ComponentTest/index.jsx";
+
+export default function PageTest() {
+  return(
+    <main>
+      <h1>Teste da página</h1>
+      <ComponentTest />
+    </main>
+  );
+}
