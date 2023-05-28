cc543bb69edc3ccbc1a49711f4b3bf08f5773a9b
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-05-28 12:14:33 -0300

    Criando pastas da home

---
+++ src/components/Home-components/Inicio.jsx
@@ -0,0 +1,13 @@
+import "../Home-components/Inicio.css"
+
+function Inicio(){
+    return(
+        <>
+            <section id="Iniciosec">
+
+            </section>
+        </>
+    )
+}
+
+export default Inicio
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import Inicio from "../../components/Home-components/Inicio"
+
+function Home(){
+    return(
+        <>
+           <Inicio/>
+        </>
+    )
+}
+
+export default Home
