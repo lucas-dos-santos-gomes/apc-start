2ef09d97e9ca93123677df21b82705063a41b54f
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-05-28 13:04:55 -0300

    Div dos botões feita

--- src/components/Home-components/Inicio.jsx
+++ src/components/Home-components/Inicio.jsx
@@ -6,7 +6,16 @@ function Inicio(){
         <section id="Iniciosec">
             <div className="Info">
                 <h1><span className="Yellow">A</span><span className="Blue">P</span><span className="Pink">C</span> é uma plataforma Web desenvolvida para criar mais <span className="Gold">oportunidades</span></h1>
+
+                <div class="buttons">
+                    <a class="btn-sobre">Sobre Nós</a>
+                    <a class="btn-acesso">Acessar a plataforma</a>
+                </div>
             </div>
+
+                <div class="Met-image">
+                    <img src="https://placehold.co/600x600" alt=""/>
+                </div>
         </section>
         </>
         
