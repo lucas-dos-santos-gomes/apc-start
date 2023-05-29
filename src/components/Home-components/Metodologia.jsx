1ea41b17032abcbd5f8408a7b1df79cbfb94c135
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-05-28 23:34:43 -0300

    Componente metodologia da home finalizado

--- src/components/Home-components/Metodologia.jsx
+++ src/components/Home-components/Metodologia.jsx
@@ -1,3 +1,8 @@
+import batepapo from "../../assets/images/home-img/bate-papo.png"
+import grafico from "../../assets/images/home-img/grafico.png"
+import homemlampada from "../../assets/images/home-img/cara-sentado-lendo.png"
+
+
 import "../Home-components/Metodologia.css"
 
 function Metodologia(){
@@ -5,33 +10,33 @@ function Metodologia(){
         <>
         <section id="Metodologiasec">
             <div className="titulo">
-            <h1>Metodologia de ensino</h1>
+                <h1>Metodologia de ensino</h1>
             </div>
             
 
             <div className="Cont-meto">
                 <div className="Info-meto">
-                <div className="dados">
-                    <img src="./src/assets/img/home/dados.png" alt=""/>
-                    <div>
-                    <h2>Baseada em <span className="Gold">dados</span></h2>
-                    <h3>Abordagem pegagógica estrategica, baseada em dados que relevam as necessidades do estudante.</h3>
+                    <div className="dados">
+                        <img src={grafico} alt=""/>
+                        <div>
+                        <h2>Baseada em <span className="Gold">dados</span></h2>
+                        <h3>Abordagem pegagógica estrategica, baseada em dados que relevam as necessidades do estudante.</h3>
                     </div>
                     
                 </div>
             
                 <div className="papo">
-                    <img src="./src/assets/img/home/bate-papo.png" alt=""/>
+                    <img src={batepapo} alt=""/>
                     <div>
-                    <h2>Alto engajamento dos <span className="Gold">alunos</span></h2>
-                    <h3>Experiências dinâmicas e de alto engajamento.</h3>
+                        <h2>Alto engajamento dos <span className="Gold">alunos</span></h2>
+                        <h3>Experiências dinâmicas e de alto engajamento.</h3>
                     </div>
                     
                 </div>
                 </div>
                 
                 <div className="img-meto">
-                <img src="./src/assets/img/home/cara-sentado-lendo.png" alt=""/>
+                    <img src={homemlampada} alt=""/>
                 </div>
             </div>
         </section>
