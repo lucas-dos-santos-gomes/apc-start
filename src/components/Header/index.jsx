48804ac19aa830c9a5f511174a50502053cd0212
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-05-30 23:23:25 -0300

    Inicianco a header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,18 @@
 function Header(){
     return(
         <>
-        
+        <header id="header-principal">
+            <nav className="nav-header">
+                <ul className="nav-list">
+                    <li><a href="">Início</a></li>
+                    <li><a href="">Metodologia</a></li>
+                    <li><a href="">Benefícios</a></li>
+                    <li><a href="">Ajuda</a></li>
+                    <li><a href="">Depoimentos</a></li> 
+                    <li><a href="">Plataforma</a></li> 
+                </ul>
+            </nav>
+        </header>
         </>
         
     )
