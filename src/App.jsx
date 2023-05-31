69ac62734fd29bf2d94731df1d824540c429978e
GabrielMota21 <motagabriel87488@gmail.com>
2023-05-30 23:25:29 -0300

    removendo pastas vazias , inserindo Area psicopeda

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <>
      <Home/>
+     <areapsicologo/>
     </>
   )
 }
--- src/components/ComponentTest/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function ComponentTest() {
-  return(
-    <div>
-      <h2>Testando um componente</h2>
-      <p>Maciel mongol</p>
-    </div>
-  );
-}
