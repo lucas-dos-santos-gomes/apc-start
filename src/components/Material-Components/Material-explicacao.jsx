df52e7fb4d0bd718d17a0f7101a853148c6045b3
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-06-05 00:15:34 -0300

    Iniciando o componente Material-explicação

---
+++ src/components/Material-Components/Material-explicacao.jsx
@@ -0,0 +1,46 @@
+import "../Material-Components/Material-explicacao.css"
+
+function MaterialExplica(){
+    return(
+        <>
+        <section id="Materiais-explicacao">
+
+            <div class="Mate-explicacao">
+                <div class="livro-imagem">
+                    <img src="/src/assets/img/material/livro-amarelo.png" alt=""/>
+                </div>
+
+                <div class="livro-texto">
+                    <h2><span class="mat-yellow">Alfabetizando com Jogos e Atividades Divertidas</span></h2>
+                    <p>Este ebook apresenta uma série de jogos e atividades lúdicas para auxiliar na alfabetização de crianças em idade escolar. Com abordagem didática e divertida, o material oferece estratégias para tornar o processo de aprendizagem mais atrativo e eficaz, além de dicas e sugestões para os professores e pais.</p>
+                </div>
+            </div>
+
+            <div class="Mate-explicacao">
+                <div class="livro-imagem">
+                    <img src="/src/assets/img/material/livro-azul.png" alt=""/>
+                </div>
+
+                <div class="livro-texto">
+                    <h2><span class="mat-blue">Alfabetizando com Jogos e Atividades Divertidas</span></h2>
+                    <p>Com esta apostila, os pais podem auxiliar seus filhos no processo de alfabetização, por meio de atividades simples e eficazes, que podem ser realizadas em casa. O material apresenta exercícios de leitura e escrita, com orientações detalhadas e dicas para tornar o aprendizado mais leve e prazeroso para a criança.</p>
+                </div>
+            </div>
+
+            <div class="Mate-explicacao">
+                <div class="livro-imagem">
+                    <img src="/src/assets/img/material/livro-vermelho.png" alt=""/>
+                </div>
+
+                <div class="livro-texto">
+                    <h2><span class="mat-red">Alfabetizando com Jogos e Atividades Divertidas</span></h2>
+                    <p>Este audiolivro leva as crianças a uma jornada lúdica pelo mundo das letras. Com narração envolvente e trilha sonora cativante, a obra apresenta histórias e curiosidades sobre o alfabeto e a linguagem escrita, contribuindo para o desenvolvimento da alfabetização e da imaginação das crianças.</p>
+                </div>
+            </div>
+        </section>
+        </>
+        
+    )
+}
+
+export default MaterialExplica
