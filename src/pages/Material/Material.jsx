a4970b35c21eb90465ebe04809f80370eca39171
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-06-05 00:26:45 -0300

    Página Materiais finalizada

--- src/pages/Material/Material.jsx
+++ src/pages/Material/Material.jsx
@@ -1,13 +1,17 @@
+import Header from "../../components/Header/Header"
 import MaterialPrincipal from "../../components/Material-Components/Material-Principal"
 import MaterialInfo from "../../components/Material-Components/Material-info"
 import MaterialExplica from "../../components/Material-Components/Material-explicacao"
+import Footer from "../../components/Footer/Footer"
 
 function Material(){
     return(
         <>
+            <Header/>
             <MaterialPrincipal/>
             <MaterialInfo/>
             <MaterialExplica/>
+            <Footer/>
         </>
         
     )
