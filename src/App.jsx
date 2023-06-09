ff03d98ff338aa6031148892359f77f33310b3d5
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-06-09 13:45:35 -0300

    Iniciando as rotas das páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
 import Home from './pages/Home'
 import './App.css'
 
 export default function App() {
   return (
     <>
-     <Home/>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/" element={ <></> } />
+          <Route path="/" element={ <></> } />
+          <Route path="/" element={ <></> } />
+          <Route path="/" element={ <></> } />
+        </Routes>
+      </Router>
+     
      <areapsicologo/>
     </>
   )
