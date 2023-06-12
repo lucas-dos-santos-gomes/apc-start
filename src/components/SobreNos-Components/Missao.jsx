975f3989e0552a67e0ea3ce115793f85604b22de
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-06-12 15:03:04 -0300

    Terceira seção finalizada

--- src/components/SobreNos-Components/Missao.jsx
+++ src/components/SobreNos-Components/Missao.jsx
@@ -13,20 +13,41 @@ function Missao(){
                 </div>
 
                 <div className="MiViVa">
-                    <div className="textos">
-                        <img src={espada} alt="" />
-                        <p>Nossa missão é proporcionar uma aventura épica de aprendizado, onde crianças com paralisia cerebral podem desvendar seu potencial. conquistar novos conhecimentos e se tornarem verdadeiros heróis de suas próprias histórias.</p>
-                    </div>
 
+                    <div className="infoQuem">
                     <div className="textos">
-                        <img src={bandeira} alt="" />
-                        <p>Nossa visão é construir um império de aprendizado onde cada criança com paralisia cerebral possa se tornar um verdadeiro herói educacional</p>
+                        <div className="Imgtextos">
+                          <img src={espada} alt="" />  
+                        </div>
+                        <div className="texto">
+                            <h2><span className="">Missão da História</span></h2>
+                          <p>Nossa missão é proporcionar uma aventura épica de aprendizado, onde crianças com paralisia cerebral podem desvendar seu potencial. conquistar novos conhecimentos e se tornarem verdadeiros heróis de suas próprias histórias.</p>  
+                        </div>
                     </div>
-
+                    </div>
+                    
+                    <div className="infoQuem">
                     <div className="textos">
-                        <img src={coracao} alt="" />
-                        <h2>Pontos d</h2>
-                        <p>Atuar de forma ética, profissional e Nossos valores são nossos verdadeiros power-ups. Inclusão, inovação, acessibilidade e comprometimento com a educação são os pilares que impulsionam nossa empresa.</p>
+                        <div className="Imgtextos">
+                            <img src={bandeira} alt="" />  
+                        </div>
+                        <div className="texto">
+                            <h2>Próximas Fases</h2>
+                            <p>Nossa visão é construir um império de aprendizado onde cada criança com paralisia cerebral possa se tornar um verdadeiro herói educacional</p>  
+                        </div>
+                    </div>
+                    </div>
+                   
+                    <div className="infoQuem">
+                    <div className="textos">
+                        <div className="Imgtextos">
+                          <img src={coracao} alt="" />  
+                        </div>
+                        <div className="texto">
+                        <h2>Pontos de virtude</h2>
+                        <p>Atuar de forma ética, profissional e Nossos valores são nossos verdadeiros power-ups. Inclusão, inovação, acessibilidade e comprometimento com a educação são os pilares que impulsionam nossa empresa.</p>  
+                        </div>
+                    </div>
                     </div>
                 </div>
             </section>
