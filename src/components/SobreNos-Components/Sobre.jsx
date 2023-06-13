147fc9cbed16524a30b3985b9a1fa894878e9853
KAYQUE MACIEL SARAIVA <kayque.msaraiva@senacsp.edu.br>
2023-06-13 12:53:29 -0300

    Iniciando a quarta seção

--- src/components/SobreNos-Components/Sobre.jsx
+++ src/components/SobreNos-Components/Sobre.jsx
@@ -1,13 +1,32 @@
 import "../SobreNos-Components/Sobre.css"
 
+import ControleVideo from "../../assets/images/SobreNos-img/controle-video.png"
 
-function QuemSomos(){
+import PessoaSegurandoPeca from "../../assets/images/SobreNos-img/pessoa-segurando-peca.png"
+
+
+function Sobre(){
     return(
         <>
             <section id="SobreSec">
+                <div className="SobreTitulo">
+                    <h1>Sobre nós</h1>
+                </div>
+
+                <div className="Contrile-img">
+                    <img src={ControleVideo} alt="" />
+                </div>
+
+                <div className="TextoSobre">
+                    <p>Somos um grupo de guerreiros apaixonados por games e educação, unidos por uma missão épica: capacitar crianças com paralisia cerebral através do poder transformador do conhecimento. Nossa equipe, formada por especialistas em desenvolvimento, design e pedagogia, combina habilidades únicas para criar uma experiência de aprendizado inovadora e envolvente.</p>
+                </div>
+
+                <div className="Peca-img">
+                    <img src={PessoaSegurandoPeca} alt="" />
+                </div>
             </section>
         </>
     )
 }
 
-export default QuemSomos
+export default Sobre
--- src/pages/SobreNos/SobreNos.jsx
+++ src/pages/SobreNos/SobreNos.jsx
@@ -1,6 +1,7 @@
 import Principal from "../../components/SobreNos-Components/Principal"
 import QuemSomos from "../../components/SobreNos-Components/QuemSomos"
 import Missao from "../../components/SobreNos-Components/Missao"
+import Sobre from "../../components/SobreNos-Components/Sobre"
 
 function SobreNos(){
     return(
@@ -8,6 +9,7 @@ function SobreNos(){
             <Principal/>
             <QuemSomos/>
             <Missao/>
+            <Sobre/>
         </>
     )
 }
