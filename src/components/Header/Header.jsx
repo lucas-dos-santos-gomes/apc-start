656a44955dc9345f13d0d5af46f52d4ad36d1a05
KAYQUE MACIEL SARAIVA <kayque.msaraiva@senacsp.edu.br>
2023-06-13 12:31:39 -0300

    Arrumando detalhes da home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ function Header(){
                 <ul className="nav-list">
                     <li><Link to="/">Início</Link></li>
                     <li><Link to="/">Metodologia</Link></li>
-                    <li><a href="">Benefícios</a></li>
+                    <li><a href="#Metodologiasec">Benefícios</a></li>
                     <li><a href="">Ajuda</a></li>
                     <li><a href="">Depoimentos</a></li> 
                     <li><a href="">Plataforma</a></li>
---
+++ src/components/SobreNos-Components/Sobre.jsx
@@ -0,0 +1,13 @@
+import "../SobreNos-Components/Sobre.css"
+
+
+function QuemSomos(){
+    return(
+        <>
+            <section id="SobreSec">
+            </section>
+        </>
+    )
+}
+
+export default QuemSomos
