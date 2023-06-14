e007c4f8407a7831520f3f26bb0c195ffa155a83
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-06-14 13:23:22 -0300

    Iniciando a sexta seção

---
+++ src/components/SobreNos-Components/Equipe.jsx
@@ -0,0 +1,14 @@
+import "../SobreNos-Components/Equipe.css"
+
+
+function Equipe(){
+    return(
+        <>
+        <section id="EquipeSec">
+            
+        </section>
+        </>
+    )
+}
+
+export default Equipe
