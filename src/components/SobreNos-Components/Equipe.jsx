1f524f11bba4820bbc95ee131ed9b8f059394787
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-06-14 14:32:30 -0300

    Página sobre nós finalizada

--- src/components/SobreNos-Components/Equipe.jsx
+++ src/components/SobreNos-Components/Equipe.jsx
@@ -1,11 +1,57 @@
 import "../SobreNos-Components/Equipe.css"
 
+import EquipeTitulo from "../../assets/images/SobreNos-img/Equipe.png"
+import perso1 from "../../assets/images/SobreNos-img/perso1.png"
+
 
 function Equipe(){
     return(
         <>
         <section id="EquipeSec">
-            
+            <div className="EquipeTitulo">
+                <img src={EquipeTitulo} alt="" />
+                <hr />
+            </div>
+
+            <div className="EquipeImgs">
+                <div className="Equipeimg">
+                    <img src={perso1} alt="" />
+                    <p>Caio Oliveira</p>
+                    <p><span className="FuncaoColor">Product Owner</span></p>
+                </div>
+                <div className="Equipeimg">
+                    <img src={perso1} alt="" />
+                    <p>Lucas Guedes</p>
+                    <p><span className="FuncaoColor">Scrum Master</span></p>
+                </div>
+                <div className="Equipeimg">
+                    <img src={perso1} alt="" />
+                    <p>Annyk De Amantis</p>
+                    <p><span className="FuncaoColor">Diretora Financeira</span></p>
+                </div>
+                <div className="Equipeimg">
+                    <img src={perso1} alt="" />
+                    <p>Brenda Gomes</p>
+                    <p><span className="FuncaoColor">Front End</span></p>
+                </div>
+            </div>
+            <div className="EquipeImgs">
+                <div className="Equipeimg">
+                    <img src={perso1} alt="" />
+                    <p>Kayque Maciel</p>
+                    <p><span className="FuncaoColor">Front End</span></p>
+                </div>
+                <div className="Equipeimg">
+                    <img src={perso1} alt="" />
+                    <p>Kath Gomes</p>
+                    <p><span className="FuncaoColor">Front End</span></p>
+                </div>
+                <div className="Equipeimg">
+                    <img src={perso1} alt="" />
+                    <p>Gabriel Silva</p>
+                    <p><span className="FuncaoColor">Front End</span></p>
+                </div>
+            </div>
         </section>
         </>
     )
--- src/pages/SobreNos/SobreNos.jsx
+++ src/pages/SobreNos/SobreNos.jsx
@@ -3,15 +3,21 @@ import QuemSomos from "../../components/SobreNos-Components/QuemSomos"
 import Missao from "../../components/SobreNos-Components/Missao"
 import Sobre from "../../components/SobreNos-Components/Sobre"
 import Jornada from "../../components/SobreNos-Components/Jornada"
+import Equipe from "../../components/SobreNos-Components/Equipe"
+import Footer from "../../components/Footer/Footer"
+import Header from "../../components/Header/Header"
 
 function SobreNos(){
     return(
         <>
+            <Header/>
             <Principal/>
             <QuemSomos/>
             <Missao/>
             <Sobre/>
             <Jornada/>
+            <Equipe/>
+            <Footer/>
         </>
     )
 }
