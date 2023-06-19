3085010fba817c1c54c33ef37306a2cf70beaae7
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-06-19 18:03:15 -0300

    Componente nivel 1 finalizado

--- src/components/Trilha-Components/Nivel1.jsx
+++ src/components/Trilha-Components/Nivel1.jsx
@@ -1,6 +1,12 @@
 import "../Trilha-Components/Nivel1.css"
 
 import Agenda from "../../assets/images/Trilha-img/Agenda.png"
+import bolavermelha from "../../assets/images/Trilha-img/bola-vermelha.png"
+import bolaverde from "../../assets/images/Trilha-img/bola-verde.png"
+import bolaazul from "../../assets/images/Trilha-img/bola-azul.png"
+import bolaroxa from "../../assets/images/Trilha-img/bola-roxa.png"
+import bolaamarela from "../../assets/images/Trilha-img/bola-amarela.png"
+import cervoheroi from  "../../assets/images/Trilha-img/cervo-superheroi.png"
 
 function Nivel1(){
     return(
@@ -18,6 +24,21 @@ function Nivel1(){
                         <a href="">GUIA</a>
                     </div>
                 </div>
+            </div>
+
+            <div className="Trilha1">
+                <div className="trilha1img">
+                    <img src={cervoheroi} alt="" />
+                </div>
+
+                <div className="bolastrilha1">
+                    <a href=""><img src={bolaazul} alt=""  id="bolaazul1"/></a>
+                    <a href=""><img src={bolaroxa} alt="" id="bolaroxa1"/></a>
+                    <a href=""><img src={bolaamarela} alt="" id="bolaamarela1"/></a>
+                    <a href=""><img src={bolaverde} alt="" id="bolaverde1"/></a>
+                    <a href=""><img src={bolavermelha} alt="" id="bolavermelha1"/></a>
+                </div>
+
             </div>
             
         </section>
