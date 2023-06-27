0a14ec872a5842baf6cf769bf2212db7042074b5
KAYQUE MACIEL SARAIVA <kayque.msaraiva@senacsp.edu.br>
2023-06-27 12:10:22 -0300

    Arrumando erros e iniciando responsivo da página Atividades

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Trilha from "./pages/TrilhaConhecimento/Trilha"
 import AreaDeEnsino from "./pages/VideoAula/VideoAula"
 import VideoAula from "./components/AreaEnsino-Components/VideoAulaComponent"
 import Atividade from "./pages/Atividade/Atividade"
+import Musica from "./pages/Musicas/Musica"
+import Jogos from "./pages/Jogos/Jogos"
 import Faq from "./pages/Faq/Faq"
 import './App.css'
 
@@ -23,6 +25,8 @@ export default function App() {
           <Route path="/AreaDeEnsino" element={ <AreaDeEnsino/> } />
           <Route path="/VideoAula" element={ <VideoAula/> } />
           <Route path="/Atividade" element={ <Atividade/> } />
+          <Route path="/Musica" element={ <Musica/> } />
+          <Route path="/Jogos" element={ <Jogos/> } />
           <Route path="/Faq" element={ <Faq/> } />
         </Routes>
       </Router>
