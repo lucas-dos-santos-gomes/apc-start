6bdd35c4909baf3dbf22efbf348e0c434e894e3a
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-07-02 20:29:45 -0300

    Colocando todos os links no autenthication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-
-// import Area from "./pages/AreaPsicopedagogia/Area"
-// import Trilha from "./pages/TrilhaConhecimento/Trilha"
-// import AreaDeEnsino from "./pages/VideoAula/VideoAula"
-// import VideoAula from "./components/AreaEnsino-Components/VideoAulaComponent"
-// import Atividade from "./pages/Atividade/Atividade";
-// import Musica from "./pages/Musicas/Musica"
-// import Jogos from "./pages/Jogos/Jogos"
-
 import Authentication from "./routes/Authentication.jsx";
 
 import './App.css'
--- src/routes/Authentication.jsx
+++ src/routes/Authentication.jsx
@@ -1,17 +1,23 @@
 import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginAluno from "../pages/LoginAluno/LoginAluno.jsx";
-import Trilha from "../pages/TrilhaConhecimento/Trilha.jsx";
+
 import Home from "../pages/Home/index.jsx";
 import SobreNos from "../pages/SobreNos/SobreNos.jsx";
 import Faq from "../pages/Faq/Faq.jsx";
+
 import Perfil from "../pages/Perfil/Acesso.jsx";
+import LoginAluno from "../pages/LoginAluno/LoginAluno.jsx";
 import LoginProfissional from "../pages/LoginProfissional/LoginProfissional.jsx";
 import CadastroProfissional from "../pages/CadastroProfissional/CadastroProfissional.jsx";
 
+import Trilha from "../pages/TrilhaConhecimento/Trilha.jsx";
+import VideoAula from "../pages/VideoAula/VideoAula.jsx";
+import Atividade from "../pages/Atividade/Atividade.jsx";
+import Musica from "../pages/Musicas/Musica.jsx"
+import Jogos from "../pages/Jogos/Jogos.jsx"
+
 const Private = ({ Item }) => {
   const signed = false;
-
   return signed > 0 ? <Item /> : <LoginAluno />;
 }
 
@@ -24,12 +30,18 @@ export default function Authentication() {
           <Route path="/SobreNos" element={ <SobreNos/> } />
           <Route path="/Faq" element={ <Faq/> } />
           <Route path="/Perfil" element={ <Perfil/> } />
-          <Route path="/LoginAluno" element={ <LoginAluno/> } />
           <Route path="/LoginProfissional" element={ <LoginProfissional/> } />
           <Route path="/CadastroProfissional" element={ <CadastroProfissional/> } />
 
-          <Route exact path="/Trilha" element={<Private item={Trilha} />} />
+          <Route exact path="/LoginAluno" element={ <LoginAluno/> } />
           <Route exact path="/CadastroAluno" element={<Private item={<Trilha />} />} />
+
+          <Route exact path="/Trilha" element={<Private item={Trilha} />} />
+          <Route exact path="/VideoAula" element={<Private item={VideoAula} />} />
+          <Route exact path="/Atividade" element={<Private item={Atividade} />} />
+          <Route exact path="/Musica" element={<Private item={Musica} />} />
+          <Route exact path="/Jogos" element={<Private item={Jogos} />} />
+
           <Route path="*" element={<Home />} />
         </Routes>
       </Fragment>
