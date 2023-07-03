4d38f1b1525f14421336ec8d840f411ebb25e09a
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-07-03 10:38:07 -0300

    Inserindo imagens dos membros do projeto

--- src/components/SobreNos-Components/Equipe.jsx
+++ src/components/SobreNos-Components/Equipe.jsx
@@ -1,7 +1,13 @@
 import "../SobreNos-Components/Equipe.css"
 
 import EquipeTitulo from "../../assets/images/SobreNos-img/Equipe.png"
-import perso1 from "../../assets/images/SobreNos-img/perso1.png"
+import Caio from "../../assets/images/SobreNos-img/Caio.jpeg"
+import Maciel from "../../assets/images/SobreNos-img/Maciel.jpeg"
+import Lucas from "../../assets/images/SobreNos-img/Lucas.jpeg"
+import Annyk from "../../assets/images/SobreNos-img/Annyk.jpeg"
+import Brenda from "../../assets/images/SobreNos-img/Brenda.jpeg"
+import Kath from "../../assets/images/SobreNos-img/Kath.jpeg"
+import Gabriel from "../../assets/images/SobreNos-img/Gabriel.jpeg"
 
 
 function Equipe(){
@@ -15,39 +21,39 @@ function Equipe(){
 
             <div className="EquipeImgs">
                 <div className="Equipeimg">
-                    <img src={perso1} alt="" />
+                    <img src={Caio} alt="" />
                     <p>Caio Oliveira</p>
                     <p><span className="FuncaoColor">Product Owner</span></p>
                 </div>
                 <div className="Equipeimg">
-                    <img src={perso1} alt="" />
+                    <img src={Lucas} alt="" />
                     <p>Lucas Guedes</p>
                     <p><span className="FuncaoColor">Scrum Master</span></p>
                 </div>
                 <div className="Equipeimg">
-                    <img src={perso1} alt="" />
+                    <img src={Annyk} alt="" />
                     <p>Annyk De Amantis</p>
                     <p><span className="FuncaoColor">Diretora Financeira</span></p>
                 </div>
                 <div className="Equipeimg">
-                    <img src={perso1} alt="" />
+                    <img src={Brenda} alt="" />
                     <p>Brenda Gomes</p>
                     <p><span className="FuncaoColor">Front End</span></p>
                 </div>
             </div>
             <div className="EquipeImgs">
                 <div className="Equipeimg">
-                    <img src={perso1} alt="" />
+                    <img src={Maciel} alt="" />
                     <p>Kayque Maciel</p>
                     <p><span className="FuncaoColor">Front End</span></p>
                 </div>
                 <div className="Equipeimg">
-                    <img src={perso1} alt="" />
+                    <img src={Kath} alt="" />
                     <p>Kath Gomes</p>
                     <p><span className="FuncaoColor">Front End</span></p>
                 </div>
                 <div className="Equipeimg">
-                    <img src={perso1} alt="" />
+                    <img src={Gabriel} alt="" />
                     <p>Gabriel Silva</p>
                     <p><span className="FuncaoColor">Front End</span></p>
                 </div>
